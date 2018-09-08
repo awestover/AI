1e95ca0bdea734ba8f0f77466f43bad0096abb28
Alek Westover <alek.westover@gmail.com>
2018-09-08 18:27:33 -0400

    fix state descriptors

--- cube/test1.py
+++ cube/test1.py
@@ -1,6 +1,6 @@
-#2x2 Rubik's Cube Solver
-#Author: Alek and Max
-#https://www.youcandothecube.com/solve-it/2-x-2-solution
+# 2x2 Rubik's Cube Solver
+# Authors: Alek and Max
+# https://www.youcandothecube.com/solve-it/2-x-2-solution
 
 import numpy as np
 from pprint import pprint
@@ -22,12 +22,12 @@ for color in COLORS:
    [3]
 """
 moves = {
-    "R":  {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":[[0,1],1]},
-    "Ri": {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":[[0,1],1]},
-    "U":  {"indpairs":[(0,4),(5,0),(2,5),(4,2)], "indflips":[0]},
-    "Ui": {"indpairs":[(4,0),(0,5),(5,2),(2,4)], "indflips":[0]},
-    "L":  {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":[[0,1],0]},
-    "Li": {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":[[0,1],0]}
+    "R":  {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":([0,1],1)},
+    "Ri": {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":([0,1],1)},
+    "U":  {"indpairs":[(0,4),(5,0),(2,5),(4,2)], "indflips":(0)},
+    "Ui": {"indpairs":[(4,0),(0,5),(5,2),(2,4)], "indflips":(0)},
+    "L":  {"indpairs":[(0,1),(1,2),(2,3),(3,0)], "indflips":([0,1],0)},
+    "Li": {"indpairs":[(1,0),(2,1),(3,2),(0,3)], "indflips":([0,1],0)}
 }
 
 algs = {}
@@ -37,17 +37,17 @@ algs["The_Cat(Website case 5)"] = algs["Fish_1"]+['U']+algs["Fish_2"]
 algs["Diagonal(Website case 7)"] = algs["Fish_1"]+algs["Fish_2"]
 algs["The_double_sides(Website case 2)"] = algs["Fish_1"]+algs["Fish_1"]
 algs["(Website case 3)"] = algs["Fish_1"]+['U']+algs["Fish_1"]
-algs["(Webiste case 6)"] =algs["Fish_1"]+['Ui']+["Fish_2"]
+algs["(Webiste case 6)"] = algs["Fish_1"]+['Ui']+["Fish_2"]
 
 states = {
-    "Fish_1": {([1, [1,0]], "Y")}
+    "Fish_1": [([1, [1,0]], "Y")]
 }
-tmp = {}
+tmp = []
 for i in range(len(COLORS)):
-    tmp.add(([i, [0, 0]], COLORS[i]))
-    tmp.add(([i, [0, 1]], COLORS[i]))
-    tmp.add(([i, [1, 0]], COLORS[i]))
-    tmp.add(([i, [1, 1]], COLORS[i]))
+    tmp.append(([i, [0, 0]], COLORS[i]))
+    tmp.append(([i, [0, 1]], COLORS[i]))
+    tmp.append(([i, [1, 0]], COLORS[i]))
+    tmp.append(([i, [1, 1]], COLORS[i]))
 states["solved"] = tmp
 
 stateOrder = ["solved", "whiteFace", "Fish_1"]
@@ -63,30 +63,36 @@ def doFormula(oldcube, formula):
         oldcube = rotate(oldcube, move)
     return oldcube
 
-
 def isState(cube, state):
     for req in state:
-        if cube[req[0][0]][req[0][1]] != req[1]:
+        if cube[req[0][0]][req[0][1][0], req[0][1][1]] != req[1]:
             return False
     return True
 
 def getState(cube):
-    for i in stateOrder:
+    for state in stateOrder:
         try:
-            if isState(states[i]):
-                return states[i]
-        except:
+            if isState(cube, states[state]):
+                return state
+        except KeyError:
             print("not done yet")
             continue
+    return "none"
 
+cube = rotate(cube, 'R')
+pprint(cube)
 while True:
     state = getState(cube)
+    print(state)
 
     if state == "solved":
-        print("the computer solved your cube. now go solve it yourself")
+        print("The computer solved your cube. Now go solve it yourself")
         break
     elif state == "Fish_1":
+        cube = doFormula(cube, algs['Fish_1'])
         break
     else:
-        print("i dont know how to do this yet")
+        print("I don't know how to do this yet")
         break
+
+pprint(cube)
